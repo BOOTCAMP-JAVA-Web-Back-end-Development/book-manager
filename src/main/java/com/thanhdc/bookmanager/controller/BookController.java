d8e8b3f3a4332378c6d56eaaa9df44bb917dae6f
thanh <thanhdc.consulting@gmail.com>
2018-12-20 21:14:12 +0700

    add Delete Book

--- src/main/java/com/thanhdc/bookmanager/controller/BookController.java
+++ src/main/java/com/thanhdc/bookmanager/controller/BookController.java
@@ -3,12 +3,13 @@ package com.thanhdc.bookmanager.controller;
 import com.thanhdc.bookmanager.model.Book;
 import com.thanhdc.bookmanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 @RequestMapping("/book")
@@ -29,6 +30,46 @@ public class BookController {
         bookService.save(book);
         modelAndView.addObject("message","New book was created succesfully");
         modelAndView.addObject("book", new Book());
+        return modelAndView;
+    }
+
+    @GetMapping("/list")
+    public ModelAndView listBook(@PageableDefault(size = 5) Pageable pageable){
+        Page<Book> books;
+        ModelAndView modelAndView = new ModelAndView("/book/list");
+        books = bookService.findAll(pageable);
+        modelAndView.addObject("books", books);
+        return modelAndView;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView formEditBook(@PathVariable int id){
+        ModelAndView modelAndView = new ModelAndView("/book/edit");
+        modelAndView.addObject("book", bookService.findById(id));
+        return modelAndView;
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView editBook(Book book){
+        ModelAndView modelAndView = new ModelAndView("/book/edit");
+        bookService.save(book);
+        modelAndView.addObject("message","Book was edited succesfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView formDeleteBook(@PathVariable int id){
+        ModelAndView modelAndView = new ModelAndView("/book/delete");
+        modelAndView.addObject("book", bookService.findById(id));
+        return modelAndView;
+    }
+
+    @PostMapping("/delete")
+    public ModelAndView deleteBook(@RequestParam("id") int id, Book book){
+        book = bookService.findById(id);
+        bookService.remove(id);
+        ModelAndView modelAndView = new ModelAndView("/book/delete");
+        modelAndView.addObject("message", "Delete Success");
         return modelAndView;
     }
 
